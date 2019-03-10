c21503d6e32afaed9d90ae4a0aa9fae3bbf2dc8c
Nicolas Laval <nicolas.laval@insee.fr>
2019-03-10 11:44:53 +0100

    Udpate endpoint configuration

--- src/config/sparql.js
+++ src/config/sparql.js
@@ -1,6 +1,8 @@
+const SPARQL_ENDPOINT_LOS = 'http://graphdb.linked-open-statistics.org/repositories';
+
 export default {
   SPARQL_ENDPOINT_INSEE: 'http://id.insee.fr/sparql',
-  SPARQL_ENDPOINT_LOS_NUTS: 'http://graphdb.linked-open-statistics.org/repositories/nuts',
-  SPARQL_ENDPOINT_IT_TOURISM: 'http://hackathon2019.ontotext.com/repositories/hackathon2019',
-  SPARQL_ENDPOINT_FR_TOURISM: 'http://hackathon2019-2.ontotext.com/repositories/hackathon2019',
+  SPARQL_ENDPOINT_LOS_NUTS: `${SPARQL_ENDPOINT_LOS}/nuts`,
+  SPARQL_ENDPOINT_IT_TOURISM: `${SPARQL_ENDPOINT_LOS}/tourism-it`,
+  SPARQL_ENDPOINT_FR_TOURISM: `${SPARQL_ENDPOINT_LOS}/tourism-fr`,
 };
