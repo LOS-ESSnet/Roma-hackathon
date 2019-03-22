3fc68b479af3b15440e0a9e063a4f1a2e542d20b
Nicolas Laval <nicolas.laval@insee.fr>
2019-03-22 10:45:48 +0100

    Update sparql.js

--- src/config/sparql.js
+++ src/config/sparql.js
@@ -2,7 +2,7 @@ const SPARQL_ENDPOINT_LOS = 'http://graphdb.linked-open-statistics.org/repositor
 
 export default {
   SPARQL_ENDPOINT_INSEE: 'http://id.insee.fr/sparql',
-  SPARQL_ENDPOINT_LOS_NUTS: `${SPARQL_ENDPOINT_LOS}/nuts`,
+  SPARQL_ENDPOINT_LOS_NUTS: `${SPARQL_ENDPOINT_LOS}/tourism-codes`,
   SPARQL_ENDPOINT_IT_TOURISM: `${SPARQL_ENDPOINT_LOS}/tourism-it`,
   SPARQL_ENDPOINT_FR_TOURISM: `${SPARQL_ENDPOINT_LOS}/tourism-fr`,
 };
